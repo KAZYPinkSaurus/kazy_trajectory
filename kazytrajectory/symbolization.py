7d7a136e3ac6363ed5d94fe983348577d984731e
kwacgit <kwacut@gmail.com>
2018-07-22 18:45:33 +0900

    sequenceファイル作成関数作成

--- kazytrajectory/symbolization.py
+++ kazytrajectory/symbolization.py
@@ -3,17 +3,22 @@ import pandas as pd
 import sys
 import glob
 import os.path
+import csv
+import datetime
 
 class Symbolization(object):
-    def __init__(self):
-        pass
-
-    def symbolize(self,dataframe,column_names,thresholds):
+    def __init__(self, column_names,thresholds):
         """
-        columns_names = ['hoge','fuga']
+        column_names = ['hoge','huga'] : columns using process
 
         thresholds = [[-3,0,2,3.5],[2,10]] :  sorted list!!!
 
+        """
+        self.columns = column_names
+        self.thresholds = thresholds
+
+    def symbolize(self,dataframe):
+        """
         return ['1:3:4','3:3:1','1:1:2',.....]
         """
 
@@ -22,7 +27,7 @@ class Symbolization(object):
         for index, row in dataframe.iterrows():
             is_first = True
             item = ''
-            for i, j in zip(column_names, thresholds):
+            for i, j in zip(self.columns, self.thresholds):
                 symbol = 0
                 for k in j:
                     if row[i] < k:
@@ -45,30 +50,36 @@ class Symbolization(object):
         
         return sequence
 
-    # def symbolize_trajectories(self,trajectories_path, save_path):
-    #     """
-    #     trajectories_path = 'hoge/trajectories/'
+    def symbolize_trajectories(self,trajectories_path, save_path):
+        """
+        trajectories_path = 'hoge/trajectories/'
 
-    #     save_path = 'hoge/maps/'
-    #     """
+        save_path = 'hoge/maps/'
+        """
 
-    #     files = glob.glob(trajectories_path + '*')
+        files = glob.glob(trajectories_path + '*')
+        save_for = save_path + 'sequence_' + '{0:%y%m%d%H%M}'.format(datetime.datetime.now()) + '.csv'
 
-    #     # for all data
-    #     for i in files:
-    #         print(i)
-    #         file_extension = i.split('.')[-1] 
-    #         # load data
-    #         if file_extension == 'tsv':
-    #             # delete all rows which has NaN
-    #             df = pd.read_table(i,header=1)
-    #             df = df.loc[:,[self.lat_name,self.long_name]].dropna()
-    #         elif file_extension == 'csv':
-    #             # delete all rows which has NaN
-    #             df = pd.read_csv(i,header=1)
-    #             df = df.loc[:,[self.lat_name,self.long_name]].dropna()
-    #         else:
-    #             continue
-            
-    #         save_for = save_path + os.path.splitext(i.split('/')[-1])[0] + '.html'
-    #         self.drow_map([df[self.lat_name], df[self.long_name]], save_for)
+        sequences = []
+        # for all data
+        for i in files:
+            print(i)
+            file_extension = i.split('.')[-1] 
+            # load data
+            if file_extension == 'tsv':
+                # delete all rows which has NaN
+                df = pd.read_table(i,header=1)
+                df = df.loc[:,self.columns].dropna()
+            elif file_extension == 'csv':
+                # delete all rows which has NaN
+                df = pd.read_csv(i,header=1)
+                df = df.loc[:,self.columns].dropna()
+            else:
+                continue
+            sequences.append(self.symbolize(df))
+        
+        # save for csv file
+        f = open(save_for, 'w')
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(sequences)
+        f.close()
