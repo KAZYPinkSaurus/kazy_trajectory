20c8aa34d61839d10dff30efa76cd1689854e829
kwacgit <kwacut@gmail.com>
2018-07-27 15:50:57 +0900

    シンボル化の例にラベル追加

--- sample.py
+++ sample.py
@@ -32,7 +32,10 @@ if __name__ == '__main__':
     # plt_hist.plot_histgram(trajectories_path,['EST_SPEED','EST_FB_ACCEL','EST_LR_ACCEL'],100,save_path)
     
     # make sequenace
-    sb1 = sb.Symbolization(['EST_SPEED', 'EST_FB_ACCEL', 'EST_LR_ACCEL'], [[5, 40, 60], [-0.05, 0.05], [-0.01, 0.01]])
+    columns = ['EST_SPEED', 'EST_FB_ACCEL', 'EST_LR_ACCEL']
+    thresholds = [[5, 40, 60], [-0.05, 0.05], [-0.01, 0.01]]
+    label = 1
+    sb1 = sb.Symbolization(columns, thresholds,label)
     sb1.symbolize_trajectories(trajectories_path, save_path)
     
     elapsed_time = time.time() - start
