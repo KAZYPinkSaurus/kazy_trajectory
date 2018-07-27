a31e56d68927c6604c38343af812cbbe2c250086
kwacgit <kwacut@gmail.com>
2018-07-27 16:02:40 +0900

    column -> columns_name

--- sample.py
+++ sample.py
@@ -32,10 +32,10 @@ if __name__ == '__main__':
     # plt_hist.plot_histgram(trajectories_path,['EST_SPEED','EST_FB_ACCEL','EST_LR_ACCEL'],100,save_path)
     
     # make sequenace
-    columns = ['EST_SPEED', 'EST_FB_ACCEL', 'EST_LR_ACCEL']
+    column_names = ['EST_SPEED', 'EST_FB_ACCEL', 'EST_LR_ACCEL']
     thresholds = [[5, 40, 60], [-0.05, 0.05], [-0.01, 0.01]]
     label = 1
-    sb1 = sb.Symbolization(columns, thresholds,label)
+    sb1 = sb.Symbolization(column_names, thresholds,label)
     sb1.symbolize_trajectories(trajectories_path, save_path)
     
     elapsed_time = time.time() - start
