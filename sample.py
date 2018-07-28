141104688e4fd3b78f0c51043407d978271222b0
kwacgit <kwacut@gmail.com>
2018-07-28 14:06:40 +0900

    highlight用のsapmle追加

--- sample.py
+++ sample.py
@@ -32,12 +32,22 @@ if __name__ == '__main__':
     # plt_hist.plot_histgram(trajectories_path,['EST_SPEED','EST_FB_ACCEL','EST_LR_ACCEL'],100,save_path)
     
     # make sequenace
-    column_names = ['EST_SPEED', 'EST_FB_ACCEL', 'EST_LR_ACCEL']
-    thresholds = [[5, 40, 60], [-0.05, 0.05], [-0.01, 0.01]]
-    label = 1
-    sb1 = sb.Symbolization(column_names, thresholds,label)
-    sb1.symbolize_trajectories(trajectories_path, save_path)
+    # column_names = ['EST_SPEED', 'EST_FB_ACCEL', 'EST_LR_ACCEL']
+    # thresholds = [[5, 40, 60], [-0.05, 0.05], [-0.01, 0.01]]
+    # label = 1
+    # sb1 = sb.Symbolization(column_names, thresholds,label)
+    # sb1.symbolize_trajectories(trajectories_path, save_path)
+
+    #highlite pattern on map
+    # pattern = ['0:1 0:1 0:1 0:1','1:2 1:2 1:2 1:2','1:0 1:0 1:0 1:0']
+    # thresholds = [[5], [-0.01,0.01]]
+    # columns = ['speed','accel']
+    # hl = vt.Visualization(name_lat_long=['latitude', 'longitude'],overlay=True)
+    # hl.highlight_patterns_on_maps(trajectories_path, pattern, columns, thresholds, save_path)
     
+    
+
+
     elapsed_time = time.time() - start
     print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
     
